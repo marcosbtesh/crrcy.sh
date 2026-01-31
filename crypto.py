8947be91ffd70335cde5b73b5ff267b8725cb1c4
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2026-01-30 22:43:14 -0300

    using sdks for cypto and fiat

--- crypto.py
+++ crypto.py
@@ -1,3 +1,15 @@
+import os
+
+from coingecko_sdk import Coingecko
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Crypto:
     def __init__(self) -> None:
+        self.CACHE_KEY = "crypto_rates"
+        self.client = Coingecko(
+            pro_api_key=os.getenv("FIAT_FREE_CURRENCY_API_KEY"), environment="demo"
+        )
         pass
--- fiat.py
+++ fiat.py
@@ -1,11 +1,18 @@
+import os
+
+import freecurrencyapi
 import requests
+from dotenv import load_dotenv
 
 from cache import get_cache, set_cache
+
+load_dotenv()
 
 
 class Fiat:
     def __init__(self) -> None:
         self.CACHE_KEY = "fiat_rates"
+        self.client = freecurrencyapi.Client(os.getenv("FIAT_FREE_CURRENCY_API_KEY"))
         pass
 
     async def get_rates(self):
@@ -16,7 +23,7 @@ class Fiat:
         if cached_data:
             return cached_data
 
-        response = await requests.get("")
+        response = self.client.currencies()
 
         rates = response["data"]
 
@@ -30,10 +37,26 @@ class Fiat:
         if cached_data:
             return cached_data
 
-        response = await requests.get("")
+        response = self.client.latest()
+
         rates = response["data"]
 
         set_cache(self.CACHE_KEY, rates)
 
-    async def get_iso_rate(self, iso: str):
-        print(f"Getting rates for {iso}")
+        return rates
+
+    async def get_iso_rate(self, isos: list[str]):
+        print(f"Getting rates for {isos}")
+
+        cached_data = get_cache(self.CACHE_KEY)
+
+        if cached_data:
+            return cached_data
+
+        response = self.client.currencies(currencies=[isos])
+
+        rates = response["data"]
+
+        set_cache(self.CACHE_KEY, rates)
+
+        return rates
