bb2c174df0304ecba887fa442b2243717d6a6ea9
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2026-01-31 01:20:32 -0300

    Update currency.py

--- currency.py
+++ currency.py
@@ -27,6 +27,7 @@ class Currency:
         base = base.upper()
         is_crypto_base = self.checker.check_which_type_of_currency(base) == "CRYPTO"
         cache_expire_hours = 2 if is_crypto_base else 6
+        cache_expire_hours = 24 if is_crypto_base else 24
 
         prefix = f"{self.CACHE_PREFIX}:{base}"
 
