bf97c95036e98a886774680aa38e12e91eeb87b4
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2026-03-16 13:12:36 -0300

    Update main.ino

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -46,7 +46,7 @@ const int LED_RED_PIN = 23;
 const float VARIATION_CHANGE_PERCENT = 0.2;
 
 // BUTTON
-const int BUTTON_PIN = 0; // change this 
+const int BUTTON_PIN = 13;
 int last_button_state = LOW;
 
 // KEYPAD
